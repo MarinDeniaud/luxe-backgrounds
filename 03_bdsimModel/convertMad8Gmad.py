a65d23085c13ad8cb27e4dc49c72d24ce7b290f9
marindeniaud <pkap009@live.rhul.ac.uk>
2022-07-19 12:41:08 +0100

    Add script to convert luxe beamline from mad8 to gmad

---
+++ 03_bdsimModel/convertMad8Gmad.py
@@ -0,0 +1,11 @@
+import pybdsim as _bd
+import pymad8 as _m8
+
+_bd.Convert.Mad82Gmad('../01_mad8/TWISS_CL_T20','T20',
+                beamparamsdict          = {'EX':3.58*10**-11,'EY':3.58*10**-11,'Esprd':1*10**-6,'particletype':'e-'},
+                #aperturedict           = {'FD0125' : {"APER_1": 0.2}},
+                #aperlocalpositions      = {1: [(0.0, {"APER_1": 0.4}),
+                #                                (0.1, {"APERTYPE": 'elliptical',"APER_1":0.2,"APER_2":0.3})]},
+                #collimdict             = {},
+                #userdict               = {'FD0125' : {'biasMaterial':'"biasDef1"'}},
+                )
