d236fd4a41f5992e2aea1141362cce7f8e8f246f
marindeniaud <pkap009@live.rhul.ac.uk>
2023-05-31 07:59:55 +0100

    Add new file to combine histograms

---
+++ 06_analysis/combinehistograms_wire.sh
@@ -0,0 +1,5 @@
+# environment - bdsim, root, geant4 etc
+source /cvmfs/beam-physics.cern.ch/bdsim/x86_64-centos7-gcc11-opt/bdsim-env-develop-g4v11.0.2-boost.sh
+
+# combine command
+while read tag; do rebdsimCombine "$tag"_merged_hist.root *"$tag"_hist.root; done < tagfilelistwire
