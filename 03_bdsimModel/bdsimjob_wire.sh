b870998c07f0f0379a26a8cbdf490d6e056ca167
marindeniaud <pkap009@live.rhul.ac.uk>
2023-06-12 16:47:40 +0100

    Update BDSIM and Geant4 versions for the farm

--- 03_bdsimModel/bdsimjob_wire.sh
+++ 03_bdsimModel/bdsimjob_wire.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # environment - bdsim, root, geant4 etc
-source /cvmfs/beam-physics.cern.ch/bdsim/x86_64-centos7-gcc11-opt/bdsim-env-develop-g4v11.0.2-boost.sh
+source /cvmfs/beam-physics.cern.ch/bdsim/x86_64-centos7-gcc11-opt/bdsim-env-v1.7.0-rc-g4v10.7.2.3-ftfp-boost.sh
 
 # echo inputs
 INPUTFILE=$1
