1d61d5701dd0130fae92b1b7ccafb2b838b2ebf2
marindeniaud <pkap009@live.rhul.ac.uk>
2023-06-12 16:43:10 +0100

    Add files for bias scan

---
+++ 03_bdsimModel/submit_multiple_jobs_wire_bias.sh
@@ -0,0 +1,12 @@
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e0.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e1.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e2.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e3.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e4.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e5.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e6.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e7.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e8.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e9.gmad seedstart=0 npart=1000 njobs=100
+condor_submit htcondor_wire.sub filename=T20_for_wire_with_offset_+0.00_bias_1e10.gmad seedstart=0 npart=1000 njobs=100
+
