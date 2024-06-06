4c4f764d56e0f1cb1ba977c9369734c298e1fa70
marindeniaud <pkap009@live.rhul.ac.uk>
2024-06-06 12:20:13 +0100

    Add shortcuts for paths and rework naming

--- 03_bdsimModel/generateGmadFilesLUXE.py
+++ 03_bdsimModel/generateGmadFilesLUXE.py
@@ -5,6 +5,17 @@ from datetime import datetime
 import subprocess as _sub
 
 
+main_path       = "~/PhD/PhD-git/luxe-backgrounds/"
+gmad_path       = "gmad_files/"
+gdml_path       = "../../gdml_files/"
+template_path   = "gmad_files/"
+beamDump_path   = gmad_path + "beamDump/"
+beamGas_path    = gmad_path + "beamGas/"
+
+
+template_default_tag = "TL-T20-FF-LUXE_template"
+
+
 Day_dict = {1: {"Name": "Monday",    "Short": "Mon"},
             2: {"Name": "Tuesday",   "Short": "Tue"},
             3: {"Name": "Wednesday", "Short": "Wed"},
@@ -28,12 +39,14 @@ Month_dict = {1:  {"Name": "January",    "Short": "Jan"},
               12: {"Name": "December",   "Short": "Dec"}}
 
 
-Twiss_dict = {"TL":  {'ALPHX': 0.000000,   'ALPHY': 0.000000,   'BETX': 0.200000,     'BETY': 3.000000,
-                      'DX': 0.0,           'DY': 0.0,           'DPX': 0.0,           'DPY': 0.0},
-              "T20": {'ALPHX': 0.704704,   'ALPHY': -2.157083,  'BETX': 11.376521,    'BETY': 42.889192,
-                      'DX': 8.975787e-17,  'DY': -0.025708,     'DPX': 5.688787e-18,  'DPY': -0.001629},
-              "FF":  {'ALPHX': -1.224729,  'ALPHY': -2.153503,  'BETX': 11.655919,    'BETY': 50.099013,
-                      'DX': -2.066821e-11, 'DY': -4.393263e-11, 'DPX': -2.244639e-12, 'DPY': -6.532824e-12}}
+Twiss_dict = {"TL":   {'ALPHX': 0.000000,   'ALPHY': 0.000000,   'BETX': 0.200000,     'BETY': 3.000000,
+                       'DX': 0.0,           'DY': 0.0,           'DPX': 0.0,           'DPY': 0.0},
+              "T20":  {'ALPHX': 0.704704,   'ALPHY': -2.157083,  'BETX': 11.376521,    'BETY': 42.889192,
+                       'DX': 8.975787e-17,  'DY': -0.025708,     'DPX': 5.688787e-18,  'DPY': -0.001629},
+              "FF":   {'ALPHX': -1.224729,  'ALPHY': -2.153503,  'BETX': 11.655919,    'BETY': 50.099013,
+                       'DX': -2.066821e-11, 'DY': -4.393263e-11, 'DPX': -2.244639e-12, 'DPY': -6.532824e-12},
+              "LUXE": {'ALPHX': 3.256904,   'ALPHY': 3.256919,   'BETX': 26.767280,    'BETY': 26.764963,
+                       'DX': -2.939631e-11, 'DY': -8.382907e-11, 'DPX': 3.983481e-12,  'DPY': 4.543499e-12}}
 
 
 def getFirstLatticeSection(lattice):
@@ -46,13 +59,12 @@ def getDateTime():
     now = datetime.now()
     dayname = Day_dict[now.isoweekday()]["Short"]
     monthname = Month_dict[now.month]["Short"]
-    date = [dayname, now.day, monthname, now.year]
+    date = [dayname, "{:0>2}".format(now.day), monthname, now.year]
     time = ["{:0>2}".format(now.hour), "{:0>2}".format(now.minute), "{:0>2}".format(now.second)]
     return date, time
 
 
-def GenerateMainGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                         templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
+def GenerateMainGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path,
                          sampleAll=True, samplerList=None):
     if sampleAll:
         samplers = "sample, all;"
@@ -60,12 +72,11 @@ def GenerateMainGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimMode
         samplers = ""
         for sampler in samplerList:
             samplers += "sample, range = {};\n".format(sampler)
-    paramdict = dict(tag=lattice+tag, dayname=date[0], day=date[1], month=date[2], year=date[3], hour=time[0], minute=time[1], second=time[2], samplers=samplers)
-    GenerateOneGmadFile(pathtooutput + lattice + tag + ".gmad", templatetag + ".gmad", templatefolder, paramdict)
+    paramdict = dict(tag=lattice+'_'+tag, dayname=date[0], day=date[1], month=date[2], year=date[3], hour=time[0], minute=time[1], second=time[2], samplers=samplers)
+    GenerateOneGmadFile(pathtooutput + lattice + '_' + tag + ".gmad", templatetag + ".gmad", templatefolder, paramdict)
 
 
-def GenerateBeamGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                         templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
+def GenerateBeamGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path,
                          distrType="gausstwiss", energy=14, particle="e-",
                          X0=0, Xp0=0, Y0=0, Yp0=0, emitx=3.58e-11, emity=3.58e-11,
                          sigmaX=10e-6, sigmaXp=10e-6, sigmaY=10e-6, sigmaYp=10e-6, sigmaT=100e-15, sigmaE=1e-6):
@@ -77,22 +88,24 @@ def GenerateBeamGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimMode
                      dispx=Twiss_dict[firstSection]['DX'], dispxp=Twiss_dict[firstSection]['DPX'],
                      dispy=Twiss_dict[firstSection]['DY'], dispyp=Twiss_dict[firstSection]['DPY'],
                      emitx=emitx, emity=emity, sigmaX=sigmaX, sigmaXp=sigmaXp, sigmaY=sigmaY, sigmaYp=sigmaYp, sigmaT=sigmaT, sigmaE=sigmaE)
-    GenerateOneGmadFile(pathtooutput + lattice + tag + "_beam.gmad", templatetag + "_beam.gmad", templatefolder, paramdict)
+    GenerateOneGmadFile(pathtooutput + lattice + '_' + tag + "_beam.gmad", templatetag + "_beam.gmad", templatefolder, paramdict)
 
 
-def GenerateComponentsGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                               templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
-                               FIELD1=0.0, FIELD2=0.0, FIELD3=0.0,  # 1.2 1.5 1.4
-                               gdmlfolder='../gdml_files/', ICSC='ICSChamber.gdml', IPCH='IPChamberWithAssembly.gdml', GAMC='GammaChamber.gdml',
-                               biasMatDump1='biasDump', biasMatDump2='biasDump', biasMatDump3='biasDump'):
+def GenerateComponentsGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path,
+                               gdmlfolder=gdml_path, ICSC='ICSChamber.gdml', M001='FirstMagnet.gdml', DUM1='FirstDump.gdml',
+                               IPCH='IPChamberWithAssembly.gdml', M002='SecondMagnet.gdml', EXT1='FirstExtraction.gdml',
+                               DET1='FirstDetectors.gdml', GAMC='GammaChamber.gdml', DUM2='SecondDump.gdml', M003='ThirdMagnet.gdml',
+                               EXT2='SecondExtraction.gdml', DUM3='ThirdDump.gdml', WALL='FinalWall.gdml',
+                               FIELD1=0.0, FIELD2=0.0, FIELD3=0.0, biasMatDump1='biasDump', biasMatDump2='biasDump', biasMatDump3='biasDump'):
     paramdict = dict(dayname=date[0], day=date[1], month=date[2], year=date[3], hour=time[0], minute=time[1], second=time[2],
-                     FIELD1=FIELD1, FIELD2=FIELD2, FIELD3=FIELD3, ICSC=gdmlfolder+ICSC, IPCH=gdmlfolder+IPCH, GAMC=gdmlfolder+GAMC,
-                     biasMatDump1=biasMatDump1, biasMatDump2=biasMatDump2, biasMatDump3=biasMatDump3)
-    GenerateOneGmadFile(pathtooutput + lattice + tag + "_components.gmad", templatetag + "_components.gmad", templatefolder, paramdict)
+                     ICSC=gdmlfolder+ICSC, M001=gdmlfolder+M001, DUM1=gdmlfolder+DUM1, IPCH=gdmlfolder+IPCH, M002=gdmlfolder+M002,
+                     EXT1=gdmlfolder+EXT1, DET1=gdmlfolder+DET1, GAMC=gdmlfolder+GAMC, DUM2=gdmlfolder+DUM2, M003=gdmlfolder+M003,
+                     EXT2=gdmlfolder+EXT2, DUM3=gdmlfolder+DUM3, WALL=gdmlfolder+WALL,
+                     FIELD1=FIELD1, FIELD2=FIELD2, FIELD3=FIELD3, biasMatDump1=biasMatDump1, biasMatDump2=biasMatDump2, biasMatDump3=biasMatDump3)
+    GenerateOneGmadFile(pathtooutput + lattice  + '_' + tag + "_components.gmad", templatetag + "_components.gmad", templatefolder, paramdict)
 
 
-def GenerateMaterialGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                             templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
+def GenerateMaterialGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path,
                              T=300, density=1e-12, compList=["G4_H", "G4_C", "G4_O"], fracList=[0.482, 0.221, 0.297]):
     components = ''
     fractions = ''
@@ -104,11 +117,10 @@ def GenerateMaterialGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsim
         raise IOError("Number of componemts should be consistant across all parameters")
     paramdict = dict(dayname=date[0], day=date[1], month=date[2], year=date[3], hour=time[0], minute=time[1], second=time[2],
                      T=T, density=density, components=components[:-2], fractions=fractions[:-2])
-    GenerateOneGmadFile(pathtooutput + lattice + tag + "_material.gmad", templatetag + "_material.gmad", templatefolder, paramdict)
+    GenerateOneGmadFile(pathtooutput + lattice + '_' + tag + "_material.gmad", templatetag + "_material.gmad", templatefolder, paramdict)
 
 
-def GenerateObjectsGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                            templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
+def GenerateObjectsGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path,
                             biasNameList=["biasElecBeamGas", "biasDump"], particleList=['e-', 'neutron'],
                             processList=[['eBrem', 'CoulombScat', 'electronNuclear'], ['neutronInelastic']],
                             xsecfactList=[[1.0, 1.0, 1.0], [1.0]], flagList=[[2, 2, 2], [1]]):
@@ -130,11 +142,10 @@ def GenerateObjectsGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimM
     else:
         raise IOError("Number of biases should be consistant across all parameters")
     paramdict = dict(dayname=date[0], day=date[1], month=date[2], year=date[3], hour=time[0], minute=time[1], second=time[2], biases=biases)
-    GenerateOneGmadFile(pathtooutput + lattice + tag + "_objects.gmad", templatetag + "_objects.gmad", templatefolder, paramdict)
+    GenerateOneGmadFile(pathtooutput + lattice + '_' + tag + "_objects.gmad", templatetag + "_objects.gmad", templatefolder, paramdict)
 
 
-def GenerateOptionsGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                            templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
+def GenerateOptionsGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path,
                             physicsList=['em', 'em_extra', 'qgsp_bert', 'decay'],
                             useLENDGammaNuclear=0, useElectroNuclear=1, useMuonNuclear=1, useGammaToMuMu=1, usePositronToMuMu=1,
                             usePositronToHadrons=1, printPhysicsProcesses=0, worldMaterial='vacuum', checkOverlaps=0, storeEloss=0,
@@ -148,19 +159,19 @@ def GenerateOptionsGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimM
                      printPhysicsProcesses=printPhysicsProcesses, worldMaterial=worldMaterial, checkOverlaps=checkOverlaps, storeEloss=storeEloss,
                      storeTrajectory=storeTrajectory, storeTrajectorySecondaryParticles=storeTrajectorySecondaryParticles, trajCutGTZ=trajCutGTZ,
                      trajectoryConnect=trajectoryConnect, storeTrajectoryProcesses=storeTrajectoryProcesses)
-    GenerateOneGmadFile(pathtooutput + lattice + tag + "_options.gmad", templatetag + "_options.gmad", templatefolder, paramdict)
+    GenerateOneGmadFile(pathtooutput + lattice + '_' + tag + "_options.gmad", templatetag + "_options.gmad", templatefolder, paramdict)
 
 
-def GenerateSequenceGmadFile(lattice, tag, date, time, pathtooutput="../03_bdsimModel/gmad_files/",
-                             templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/"):
+def GenerateSequenceGmadFile(lattice, tag, date, time, pathtooutput=gmad_path, templatetag=template_default_tag, templatefolder=template_path):
     line = ''
     for section in lattice.split('-'):
-        line += section + ', '
+        if section in Twiss_dict.keys():
+            line += section + ', '
     paramdict = dict(dayname=date[0], day=date[1], month=date[2], year=date[3], hour=time[0], minute=time[1], second=time[2], line=line[:-2])
-    GenerateOneGmadFile(pathtooutput + lattice + tag + "_sequence.gmad", templatetag + "_sequence.gmad", templatefolder, paramdict)
+    GenerateOneGmadFile(pathtooutput + lattice + '_' + tag + "_sequence.gmad", templatetag + "_sequence.gmad", templatefolder, paramdict)
 
 
-def GenerateOneGmadFile(gmadfilename, templatefilename, templatefolder="../03_bdsimModel/", paramdict=None):
+def GenerateOneGmadFile(gmadfilename, templatefilename, templatefolder=template_path, paramdict=None):
     env = _jj.Environment(loader=_jj.FileSystemLoader(templatefolder))
     template = env.get_template(templatefilename)
     if paramdict is None:
@@ -170,11 +181,11 @@ def GenerateOneGmadFile(gmadfilename, templatefilename, templatefolder="../03_bd
     f.close()
 
 
-def GenerateSetGmadFiles(lattice="TL-T20-FF-LUXE", tag="_default_tag", pathtooutput="../03_bdsimModel/gmad_files/",
-                         templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/",
-                         sampleAll=True, samplerList=None,
-                         distrType="gausstwiss", energy=14, particle="e-", FIELD1=0.0, FIELD2=0.0, FIELD3=0.0,  # 1.2 1.5 1.4
-                         gdmlfolder='../gdml_files/', ICSC='ICSChamber.gdml', IPCH='IPChamberWithAssembly.gdml', GAMC='GammaChamber.gdml',
+def GenerateSetGmadFiles(lattice="TL-T20-FF-LUXE", tag="default-tag", pathtooutput=gmad_path,
+                         templatetag=template_default_tag, templatefolder=template_path,
+                         sampleAll=True, samplerList=None, distrType="gausstwiss", energy=14, particle="e-",
+                         FIELD1=0.0, FIELD2=0.0, FIELD3=0.0, gdmlfolder=gdml_path,
+                         ICSC='ICSChamber.gdml', IPCH='IPChamberWithAssembly.gdml', GAMC='GammaChamber.gdml',
                          biasMatDump1='biasDump', biasMatDump2='biasDump', biasMatDump3='biasDump',
                          T=300, density=1e-12, compList=["G4_H", "G4_C", "G4_O"], fracList=[0.482, 0.221, 0.297],
                          biasNameList=["biasElecBeamGas", "biasDump"], particleList=['e-', 'neutron'],
@@ -187,8 +198,9 @@ def GenerateSetGmadFiles(lattice="TL-T20-FF-LUXE", tag="_default_tag", pathtoout
     date, time = getDateTime()
     GenerateMainGmadFile(lattice, tag, date, time, pathtooutput, templatetag, templatefolder, sampleAll, samplerList)
     GenerateBeamGmadFile(lattice, tag, date, time, pathtooutput, templatetag, templatefolder, distrType, energy, particle)
-    GenerateComponentsGmadFile(lattice, tag, date, time, pathtooutput, templatetag, templatefolder, FIELD1, FIELD2, FIELD3,
-                               gdmlfolder, ICSC, IPCH, GAMC, biasMatDump1, biasMatDump2, biasMatDump3)
+    GenerateComponentsGmadFile(lattice, tag, date, time, pathtooutput, templatetag, templatefolder, gdmlfolder,
+                               ICSC=ICSC, IPCH=IPCH, GAMC=GAMC, FIELD1=FIELD1, FIELD2=FIELD2, FIELD3=FIELD3,
+                               biasMatDump1=biasMatDump1, biasMatDump2=biasMatDump2, biasMatDump3=biasMatDump3)
     GenerateMaterialGmadFile(lattice, tag, date, time, pathtooutput, templatetag, templatefolder, T, density, compList, fracList)
     GenerateObjectsGmadFile(lattice, tag, date, time, pathtooutput, templatetag, templatefolder,
                             biasNameList, particleList, processList, xsecfactList, flagList)
@@ -201,13 +213,34 @@ def GenerateSetGmadFiles(lattice="TL-T20-FF-LUXE", tag="_default_tag", pathtoout
     return lattice+tag
 
 
-def GenerateSetGmadFirstDump(lattice="==-===-FF-LUXE", tag="first_dump", pathtooutput="../03_bdsimModel/gmad_files/beamDump",
-                             templatetag="TL-T20-FF-LUXE_template", templatefolder="../03_bdsimModel/gmad_files/", **otherargs):
+def GenerateSetGmadFirstDump(lattice="==-===-FF-LUXE", tag="first-dump", pathtooutput=beamDump_path,
+                             templatetag=template_default_tag, templatefolder=template_path, **otherargs):
     GenerateSetGmadFiles(lattice=lattice, tag=tag, pathtooutput=pathtooutput, templatetag=templatetag, templatefolder=templatefolder,
-                         FIELD1=1.2, FIELD2=0, FIELD3=0, **otherargs)  # 1.2 1.5 1.4
+                         FIELD1=1.2, FIELD2=0, FIELD3=0, **otherargs)  # 1.2 -1.5 1.4
 
 
-def GenerateMultipleGmadSetsAndList(tag="T20_needle", tagfilename='tagfilelistneedle', valuetoscan='needleOffsetX',
+def GenerateSetGmadSecondDump(lattice="==-===-FF-LUXE", tag="second-dump", pathtooutput=beamDump_path,
+                              templatetag=template_default_tag, templatefolder=template_path, **otherargs):
+    GenerateSetGmadFiles(lattice=lattice, tag=tag, pathtooutput=pathtooutput, templatetag=templatetag, templatefolder=templatefolder,
+                         FIELD1=0, FIELD2=-1.5, FIELD3=0, **otherargs)  # 1.2 -1.5 1.4
+
+
+def GenerateSetGmadThirdDump(lattice="==-===-FF-LUXE", tag="third-dump", pathtooutput=beamDump_path,
+                             templatetag=template_default_tag, templatefolder=template_path, **otherargs):
+    GenerateSetGmadFiles(lattice=lattice, tag=tag, pathtooutput=pathtooutput, templatetag=templatetag, templatefolder=templatefolder,
+                         FIELD1=0, FIELD2=0, FIELD3=1.4, **otherargs)  # 1.2 -1.5 1.4
+
+
+def GenerateSetGmadBeamGas(lattice="TL-T20-FF-====", tag="beam-gas-biased", pathtooutput=beamGas_path,
+                           templatetag=template_default_tag, templatefolder=template_path,
+                           xsecfact=[25000000.0, 1050000000000.0, 370000000000.0], **otherargs):
+    GenerateSetGmadFiles(lattice=lattice, tag=tag, pathtooutput=pathtooutput, templatetag=templatetag, templatefolder=templatefolder,
+                         biasNameList=["biasElecBeamGas", "biasDump"], particleList=['e-', 'neutron'],
+                         processList=[['eBrem', 'CoulombScat', 'electronNuclear'], ['neutronInelastic']],
+                         xsecfactList=[xsecfact, [1.0]], flagList=[[2, 2, 2], [1]], **otherargs)
+
+
+def GenerateMultipleGmadSetsAndList(tag="T20-needle", tagfilename='tagfilelistneedle', valuetoscan='needleOffsetX',
                                     valuelist=['-0.50', '-0.40', '-0.30', '-0.20', '-0.10', '+0.00', '+0.10', '+0.20', '+0.30', '+0.40', '+0.50'],
                                     **otherargs):
     taglist = open(tagfilename, "w")
