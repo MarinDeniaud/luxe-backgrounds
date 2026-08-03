d8f6f6697fe68a8c58b4a9d898ab9858e59e94e8
marindeniaud <pkap009@live.rhul.ac.uk>
2026-08-03 13:57:33 +0100

    Add and modify functions to plot EUXFEL data

--- 06_analysis/plotXFELdata.py
+++ 06_analysis/plotXFELdata.py
@@ -232,23 +232,48 @@ def plotReconstructSignalSVDDifference(xfel_data, df_reduced, inSubplots=False, 
 
 
 def plotAverageJitterPerBunchID(xfel_data, bpms=['BPMI.1910.TL', 'BPMI.1925.TL', 'BPMI.1930.TL', 'BPMI.1939.TL'],
-                                inSubplots=False, figsize=[12, 5]):
-    bunchIDs, sx_mean, sy_mean = xfel_data.calcAverageJitterPerBunchID(bpms=bpms)
-    if not inSubplots:
+                                sample=10, addBunchPattern=False, figsize=[12, 5]):
+    bunchIDs, sx_mean, sy_mean = xfel_data.calcAverageJitterPerBunchID(bpms=bpms, sample=sample)
+    if addBunchPattern:
+        _plt.subplots(2, 1, height_ratios=[1, 4], sharex=True, figsize=figsize)
+        _plt.subplot(211)
+        ax = _plt.gca()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        xfel_data.plotBunchPattern2(train=0, sample=49, inSubplot=True)
+        _plt.subplot(212)
+    else:
         _plt.figure(figsize=figsize)
-        _plt.xlabel(r"$s$ [m]")
-    labelT1 = 'XTD1'
-    labelT2 = 'XTD2'
-    for bunchT1 in xfel_data.bunchIDs_XTD1[::10]:
-        _plt.axvline(bunchT1, color='C2', alpha=0.3, label=labelT1)
-        labelT1 = None
-    for bunchT2 in xfel_data.bunchIDs_XTD2[::5]:
-        _plt.axvline(bunchT2, color='C3', alpha=0.3, label=labelT2)
-        labelT2 = None
+    _plt.xlabel('Bunch ID')
     _plt.plot(bunchIDs, sx_mean, color='C0', label=r"$\frac{\sigma_{J,x}}{\sigma_x}$")
     _plt.plot(bunchIDs, sy_mean, color='C1', label=r"$\frac{\sigma_{J,y}}{\sigma_y}$")
     _plt.ylabel(r"$\frac{\sigma_{J}}{\sigma}$ [%]")
     _plt.legend(loc=9)
+    _plt.grid()
+    _plt.tight_layout()
+
+
+def plotAverageChargeJitterPerBunchID(xfel_data, bpms=['BPMI.1910.TL', 'BPMI.1925.TL', 'BPMI.1930.TL', 'BPMI.1939.TL'],
+                                      sample=10, addBunchPattern=False, figsize=[12, 5]):
+    bunchIDs, jq_mean = xfel_data.calcAverageChargeJitterPerBunchID(bpms=bpms, sample=sample)
+    if addBunchPattern:
+        _plt.subplots(2, 1, height_ratios=[1, 4], sharex=True, figsize=figsize)
+        _plt.subplot(211)
+        ax = _plt.gca()
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        xfel_data.plotBunchPattern2(train=0, sample=49, inSubplot=True)
+        _plt.subplot(212)
+    else:
+        _plt.figure(figsize=figsize)
+    _plt.xlabel('Bunch ID')
+    _plt.plot(bunchIDs, _np.array(jq_mean)*1e12, color='C2', label=r"$\sigma_{J,q}$")
+    _plt.ylabel(r"$\sigma_{J,q}$ [pC]")
+    _plt.legend(loc=9)
+    _plt.grid()
+    _plt.tight_layout()
 
 
 def plotOneHist(ax, df_reduced, coord='X', label="x", unit=r"$\rm \mu m$", minCut=None, maxCut=None, nbins=50):
@@ -271,8 +296,8 @@ def plotOneAlong(ax, df_reduced, slist, xlabel, coord='X', label='x', unit=r"$\r
     X_cut = df_reduced[coord][minCut:maxCut]
     ax.plot(slist, X, color='gray')
     ax.plot(slist[minCut:maxCut], X_cut, color='C0',
-            label=r"$\overline{{{}}}$ = {:.3f} {}".format(label, _np.mean(X_cut), unit))
-    ax.set_ylabel("{} [{}]".format(label, unit))
+            label=r"$\langle {} \rangle$ = {:.2f} {}".format(label, _np.mean(X_cut), unit))
+    ax.set_ylabel(r"${}$ [{}]".format(label, unit))
     ax.set_xlabel(xlabel)
     ax.legend()
 
@@ -291,6 +316,9 @@ def PlotAlong(ax_x, ax_y, df_reduced, bpms=None, trains=None, bunches=None, minC
     Y = df_reduced['Y'] * 1e6
     X_cut = df_reduced['X'][minCut:maxCut] * 1e6
     Y_cut = df_reduced['Y'][minCut:maxCut] * 1e6
+    # TODO : REMOVE PRINTS
+    print(min(X_cut), max(X_cut))
+    print(min(Y_cut), max(Y_cut))
     if bpms is None and trains is not None and bunches is not None:
         slist = df_reduced.index.get_level_values('BPM')
     elif bpms is not None and trains is None and bunches is not None:
@@ -302,8 +330,8 @@ def PlotAlong(ax_x, ax_y, df_reduced, bpms=None, trains=None, bunches=None, minC
         raise ValueError("Must use exactly 2 out of 3 between bpms, trains and bunches")
     ax_x.plot(slist, X, color='gray')
     ax_y.plot(slist, Y, color='gray')
-    ax_x.plot(slist[minCut:maxCut], X_cut, color='C0', label=r"$\overline{{x}}$ = {:.2f} $\rm \mu m$".format(_np.mean(X_cut)))
-    ax_y.plot(slist[minCut:maxCut], Y_cut, color='C1', label=r"$\overline{{y}}$ = {:.2f} $\rm \mu m$".format(_np.mean(Y_cut)))
+    ax_x.plot(slist[minCut:maxCut], X_cut, color='C0', label=r"$\langle x \rangle$ = {:.2f} $\rm \mu m$".format(_np.mean(X_cut)))
+    ax_y.plot(slist[minCut:maxCut], Y_cut, color='C1', label=r"$\langle y \rangle$ = {:.2f} $\rm \mu m$".format(_np.mean(Y_cut)))
 
 
 def PlotAll(df, bpms=None, trains=None, bunches=None,
@@ -363,8 +391,10 @@ def PlotAll(df, bpms=None, trains=None, bunches=None,
     _plt.tight_layout()
 
 
-def plotTime(df, bpms=None, trains=None, bunches=None, minCut=None, maxCut=None, figsize=[9, 6]):
-    df_reduced = XFEL_BPM.reduceDFbyBPMTrainBunchByIndex(df, bpms=bpms, trains=trains, bunches=bunches)
+def plotTime(df, bpms="BAM", trains=None, bunches=None, nbins=50,
+            drawing=None, arrow_xy=[425, 30, 0, 170],
+            hist_ylim=None, hist_xlim=None, minCut=None, maxCut=None, figsize=[9, 6]):
+    df_reduced = XFEL_BPM.reduceDFbyBPMTrainBunchByIndex(df, bpms="BPMA.1873.TL", trains=trains, bunches=bunches)
     if bpms is None and trains is not None and bunches is not None:
         xlabel = 'BPMs'
         slist = df_reduced.index.get_level_values('BPM')
@@ -379,14 +409,30 @@ def plotTime(df, bpms=None, trains=None, bunches=None, minCut=None, maxCut=None,
         raise ValueError("Must use exactly 2 out of 3 between bpms, trains and bunches")
 
     df_reduced.TimeS = df_reduced.TimeS*1e12
-
     fig = _plt.figure(figsize=figsize)
-    gs = fig.add_gridspec(1, 2)
-    ax_left = fig.add_subplot(gs[0, 0])
-    ax_right = fig.add_subplot(gs[0, 1])
+
+    if drawing is not None:
+        img = convert_from_path(drawing, dpi=300)[0]
+        gs = fig.add_gridspec(2, 2, height_ratios=[1, 3])
+        ax_pdf = fig.add_axes([0, 0.75, 1, 0.2])
+        ax_pdf.imshow(img)
+        ax_pdf.axis('off')
+        arrow = _patch.Rectangle((arrow_xy[0], arrow_xy[1]), arrow_xy[2], arrow_xy[3], lw=1.8, fill=False, color='r')
+        ax_pdf.add_patch(arrow)
+        ax_pdf.text(arrow_xy[0]+arrow_xy[2]/2, arrow_xy[1]-9, bpms, color='r', ha='center', va='bottom', fontsize=9)
+        ax_left = fig.add_subplot(gs[1, 0])
+        ax_right = fig.add_subplot(gs[1, 1])
+
+    else:
+        gs = fig.add_gridspec(1, 2)
+        ax_left = fig.add_subplot(gs[0, 0])
+        ax_right = fig.add_subplot(gs[0, 1])
 
     plotOneAlong(ax_left, df_reduced, slist, xlabel, coord='TimeS', label="t", unit='ps', minCut=minCut, maxCut=maxCut)
-    plotOneHist(ax_right, df_reduced, coord='TimeS', label="t", unit='ps', minCut=minCut, maxCut=maxCut)
+    plotOneHist(ax_right, df_reduced, coord='TimeS', label="t", unit='ps', minCut=minCut, maxCut=maxCut, nbins=nbins)
+    ax_right.set_ylim(hist_ylim)
+    ax_right.set_xlim(hist_xlim)
+    _plt.tight_layout()
 
 
 def plotBunchProfile(crisp_data, train=0, figsize=[10, 8]):
@@ -404,8 +450,8 @@ def plotBunchProfileAndCumsum(crisp_data, train=0, percentile=0.95, xlim=None, f
     _plt.subplot(121)
     time = crisp_data.df.Time
     colname = crisp_data.df.columns[1+train]
-    _plt.plot(time, crisp_data.df[colname], label=crisp_data.df[colname].name)
-    _plt.ylabel('Current')
+    _plt.plot(time, crisp_data.df[colname]*1e-3, label=crisp_data.df[colname].name)
+    _plt.ylabel('Current [kA]')
     _plt.xlabel('$t$ [fs]')
     _plt.legend()
     _plt.xlim(xlim)
@@ -444,14 +490,14 @@ def plotLengthAndHistPer(self, percentile=0.95, bins=50, minCut=None, maxCut=Non
     _plt.subplot(121)
     _plt.plot(trainIDs, Lengths, color='gray')
     _plt.plot(trainIDs_cut, Lengths_cut, color='C0', label=r"$\overline{{\sigma_t}}$ = {:.2f} fs".format(_np.mean(Lengths_cut)))
-    _plt.ticklabel_format(axis="x", style="sci", scilimits=(3, 3))
+    # _plt.ticklabel_format(axis="x", style="sci", scilimits=(3, 3))
     _plt.ylabel(r"$\sigma_t$ [fs]")
     _plt.xlabel('Train ID')
     _plt.legend()
 
     _plt.subplot(122)
     _plt.hist(Lengths_cut, bins=bins, histtype='step', color='C0', label=r"$\delta\sigma_t$ = {:.2f} fs".format(_np.std(Lengths_cut)))
-    _plt.ticklabel_format(axis="y", style="sci", scilimits=(3, 3))
+    # _plt.ticklabel_format(axis="y", style="sci", scilimits=(3, 3))
     _plt.ylabel('counts')
     _plt.xlabel(r"$\sigma_t$ [fs]")
     _plt.legend()
